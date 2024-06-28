15500d9cb9ef8d4c94406366c78fbe3d3ecfe1a7
danieltn889 <123164326+danieltn889@users.noreply.github.com>
2024-06-28 12:20:20 +0200

    21RP03122_FIVE

--- app/src/main/java/com/example/a21rp03122/DatabaseHelper.java
+++ app/src/main/java/com/example/a21rp03122/DatabaseHelper.java
@@ -7,17 +7,25 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "expensetrack.db";
+    private static final String TABLE_NAME = "expenses";
+    private static final String COLUMN_ID = "ID";
+    private static final String COLUMN_TYPE = "TYPE";
+    private static final String COLUMN_QUANTITY= "QUANTITY";
+    private static final String COLUMN_UNIT_PRICE = "UNIT_PRICE";
+    private static final String COLUMN_DATE = "DATE";
     public DatabaseHelper(@Nullable Context context) {
-        super(context, "", factory, version);
+        super(context, "DATABASE_NAME", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, TYPE TEXT, QUANTITY REAL, UNIT_PRICE REAL, DATE TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
     }
 }
--- app/src/main/java/com/example/a21rp03122/NewLogConsuptionActivity.java
+++ app/src/main/java/com/example/a21rp03122/NewLogConsuptionActivity.java
@@ -1,6 +1,8 @@
 package com.example.a21rp03122;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class NewLogConsuptionActivity extends AppCompatActivity {
-
+    EditText editQunatity,editUnitPrice;
+    Spinner
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_log_consuption);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
