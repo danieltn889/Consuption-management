0d7542c8917c37fa7773da67769d099bbcd21fa0
danieltn889 <123164326+danieltn889@users.noreply.github.com>
2024-06-28 12:30:28 +0200

    21RP03122_SIX

--- app/src/main/java/com/example/a21rp03122/NewLogConsuptionActivity.java
+++ app/src/main/java/com/example/a21rp03122/NewLogConsuptionActivity.java
@@ -1,6 +1,8 @@
 package com.example.a21rp03122;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -12,12 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NewLogConsuptionActivity extends AppCompatActivity {
     EditText editQunatity,editUnitPrice;
-    Spinner
+    Spinner spinnerConsuption;
+    Button addConsuption;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_log_consuption);
+        editQunatity=findViewById(R.id.editQuantity);
+        editUnitPrice=findViewById(R.id.editUnityPrice);
+        addConsuption=findViewById(R.id.addConsuptionButton);
+        addConsuption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
 }
